dd57996e59e04db67a9af7582fc07ad3fd27780c
Ashley da Silva Aquino <ashleysilvaaquino@gmail.com>
2022-06-01 21:57:34 -0300

    prova completaa

--- api/src/controller/usuarioController.js
+++ api/src/controller/usuarioController.js
@@ -1,22 +1,36 @@
 import { login } from '../repository/usuarioRepository.js'
-import { Router } from 'express'
-const server = Router();
- 
 
-server.post('./usuario/login', async(req,resp) => {
-     try {
-          const {email, senha} = req.body;
-          const resposta = await login(email, senha);   
-          resp.send(resposta)
-          if(resposta) {
-               throw new Error('Credenciais inválidas')
-          }
-     } 
-     catch (err) {
-         resp.status(400).send({
-              erro : 'Ocorreu um erro'
-         });
-     }
+import { Router } from 'express';
+
+const server = Router();
+
+server.post('/usuario/login', async (req, resp) => {
+
+try {
+
+const { email, senha }= req.body;
+
+const resposta =  await login(email, senha);
+
+if(!resposta) {
+
+throw new Error('Creddenciais inválidas')
+
+}
+
+resp.send(resposta)
+
+}catch(err){
+
+resp.status(401).send({
+
+erro: err.message
+
+});
+
+}
+
 })
+
 export default server;
 
--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,18 @@
 import 'dotenv/config'
-import { con } from './repository/connection.js'
-import usuarioController from './controller/usuarioController.js' 
 import express from 'express'
 import cors from 'cors'
+import filmeController from './controller/filmeController.js'
+import usuarioController from './controller/usuarioController.js'
+import { con } from'./repository/connection.js'
+
+
 
 const server = express();
+
 server.use(cors());
 server.use(express.json());
-
-// configuração dos endpoinst
+server.use('/storage/capasFilmes', express.static('storage/capasFilmes'));
 server.use(usuarioController);
-
-server.listen(process.env.PORT, () => console.log(`API CONECTADA na porta ${process.env.PORT}`));
+server.use(filmeController);
+server.listen(process.env.PORT, ()  => console.log(`Api conectada na porta ${process.env.PORT}`));
+console.log('BD conectado!');
